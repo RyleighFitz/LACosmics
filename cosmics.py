8d73f0869e7a69116021cee9612f935c1795e524
RyleighFitz <37059347+RyleighFitz@users.noreply.github.com>
2018-12-17 14:50:54 -0700

    Fixing indent error (line 716)

--- cosmics.py
+++ cosmics.py
@@ -713,16 +713,16 @@ def rebin(a, newshape):
 			>>> a=rand(6,4); b=rebin(a,(3,2))
 		"""
 		
-		shape = a.shape
-		lenShape = len(shape)
-		factor = np.asarray(shape)/np.asarray(newshape)
-		#print factor
-		evList = ['a.reshape('] + \
-				 ['newshape[%d],factor[%d],'%(i,i) for i in xrange(lenShape)] + \
-				 [')'] + ['.sum(%d)'%(i+1) for i in xrange(lenShape)] + \
-				 ['/factor[%d]'%i for i in xrange(lenShape)]
+	shape = a.shape
+	lenShape = len(shape)
+	factor = np.asarray(shape)/np.asarray(newshape)
+	#print factor
+	evList = ['a.reshape('] + \
+			 ['newshape[%d],factor[%d],'%(i,i) for i in xrange(lenShape)] + \
+			 [')'] + ['.sum(%d)'%(i+1) for i in xrange(lenShape)] + \
+			 ['/factor[%d]'%i for i in xrange(lenShape)]
 
-		return eval(''.join(evList))
+	return eval(''.join(evList))
 
 
 def rebin2x2(a):
